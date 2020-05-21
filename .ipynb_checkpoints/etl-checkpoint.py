bbf826da64c63a709db97e964a02da396258bbd4
Utsav Shah <utsav@Utsavs-MBP.lan1>
2020-05-21 01:27:27 -0400

    added README, added SERIAL in songplay_id create statement, and added a conflict action in songplay_table_insert

--- .ipynb_checkpoints/etl-checkpoint.py
+++ .ipynb_checkpoints/etl-checkpoint.py
@@ -6,6 +6,13 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """Reads songs log file row by row, selects needed fields and inserts them into song and artist tables.
+
+        Parameters:
+            cur (psycopg2.cursor()): Cursor of the sparkifydb database
+            filepath (str): Filepath of the file to be analyzed
+    """
+    
     # open song file
     df = pd.read_json(filepath, lines=True)    
     
@@ -22,6 +29,14 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
+    """Reads user activity log file row by row, filters by NexSong, selects needed fields, transforms them and inserts
+    them into time, user and songplay tables.
+
+            Parameters:
+                cur (psycopg2.cursor()): Cursor of the sparkifydb database
+                filepath (str): Filepath of the file to be analyzed
+    """
+    
     # open log file
     df = pd.read_json(filepath, lines=True)
 
@@ -63,6 +78,18 @@ def process_log_file(cur, filepath):
 
 
 def process_data(cur, conn, filepath, func):
+    """Walks through all files nested under filepath, and processes all logs found.
+
+    Parameters:
+        cur (psycopg2.cursor()): Cursor of the sparkifydb database
+        conn (psycopg2.connect()): Connectio to the sparkifycdb database
+        filepath (str): Filepath parent of the logs to be analyzed
+        func (python function): Function to be used to process each log
+
+    Returns:
+        Name of files processed
+    """
+    
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -84,6 +111,10 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    """Function used to extract, transform all data from song and user activity logs and load it into a PostgreSQL DB
+        Usage: python etl.py
+    """
+    
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb")
     cur = conn.cursor()
 
--- sql_queries.py
+++ sql_queries.py
@@ -10,7 +10,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays
-    (songplay_id int PRIMARY KEY, 
+    (songplay_id int SERIAL PRIMARY KEY, 
     start_time date REFERENCES time(start_time), 
     user_id int REFERENCES users(user_id), 
     level text, 
@@ -78,7 +78,7 @@ user_table_insert = ("""
     INSERT INTO users
     (user_id, first_name, last_name, gender, level)
     VALUES(%s, %s, %s, %s, %s)
-    ON CONFLICT (user_id) DO NOTHING
+    ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
 """)
 
 song_table_insert = ("""
